6d6bae46af33b9bec457623d3d881b5377c690f9
faaiz251 <mohdfaaizazeem@gmail.com>
2025-02-19 00:29:40 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,33 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MainNav } from './components/common/mainnav'
+import { Home } from './components/landing/Home'
 
 function App() {
 
   return (
+    <div
+    style={{
+      position: "fixed",
+      top: 0,
+      left: 0,
+      width: "100vw",
+      height: "100vh",
+      fontFamily: "cursive",
+      margin: 0,
+      padding: 0,
+      background: "linear-gradient(135deg, #2d2d2d, #1a1a1a)",
+      overflow: "hidden",
+    }}
+  >
     <BrowserRouter>
     <MainNav />
     <Routes>
       <Route path="/" element={<Home />} />
     </Routes>
   </BrowserRouter>
+  </div>
   )
+
 }
 
 export default App
--- frontend/src/components/common/mainnav.jsx
+++ frontend/src/components/common/mainnav.jsx
@@ -1,49 +1,74 @@
-import { NavLink } from "react-router-dom";
-import logo from "../../assets/672-6724074_transparent-tm-logo-png-logo-trademark-symbol-png.png";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export function MainNav() {
-  const getLinkClass = ({ isActive }) =>
-    isActive
-      ? "text-[#04284F] underline font-bold decoration-[5px] decoration-[#15549A] underline-offset-[26px]"
-      : "text-[#04284F]";
-
+  const navigate = useNavigate();
   return (
-    <nav className="py-3  flex justify-around items-center text-sm font-semibold">
-      <div className="flex items-center h-16 ml-20">
-        <NavLink to="/" className="text-2xl font-bold text-[#15549A]">
-          <img
-            height={85}
-            width={146}
-            src={logo}
-            alt="Eze Credit Repair Logo"
-            className="h-20"
-          />
-        </NavLink>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        color: "#fff",
+        fontFamily: "Arial, sans-serif",
+        padding: '50 100px'
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: "70px 150px",
+        }}
+      >
+        <div className='main-header'
+        style={{ display: "flex", alignItems: "center" }}>
+          <div
+            style={{
+              fontWeight: "bold",
+              fontSize: "26px",
+              marginRight: "10px",
+              color: "#f9a825",
+              cursor: "pointer",
+            }}
+          >
+            Quiz
+          </div>
+          <div
+            style={{
+              fontSize: "15px",
+              color: "#000",
+              fontWeight: "bold",
+              cursor: "pointer",
+            }}
+          >
+           Master
+          </div>
+        </div>
+        <div className="header2"
+          style={{
+            display: "flex",
+            gap: "100px",
+          }}
+        >
+          <div
+            onClick={() => {
+              navigate("/signup");
+            }}
+            style={{ cursor: "pointer"}}
+          >
+            Login
+          </div>
+          <div
+            onClick={() => {
+              navigate("/signin");
+            }}
+            style={{ cursor: "pointer"}}
+          >
+           Register
+          </div>
+        </div>
       </div>
-      <div className="flex space-x-8 text-xl text-[#04284F]">
-        <NavLink to="/" className={getLinkClass}>
-          Home
-        </NavLink>
-        <NavLink to="/about" className={getLinkClass}>
-          About Us
-        </NavLink>
-        <NavLink to="/services" className={getLinkClass}>
-          Our Services
-        </NavLink>
-        <NavLink to="/pricing" className={getLinkClass}>
-          Pricing Page
-        </NavLink>
-        <NavLink to="/how-it-works" className={getLinkClass}>
-          How It Works
-        </NavLink>
-        <NavLink to="/f&q" className={getLinkClass}>
-          FAQ
-        </NavLink>
-        <NavLink to="/contact" className={getLinkClass}>
-          Contact Us
-        </NavLink>
-      </div>
-     
-    </nav>
+    </div>
   );
 }
